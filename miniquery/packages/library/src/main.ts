56f3c4893bc5a89c79c434fa5427cce2e0af4953
lurgi <wahoo9040@naver.com>
2023-08-24 13:04:37 +0900

    class vs function

--- miniquery/packages/library/src/main.ts
+++ miniquery/packages/library/src/main.ts
@@ -1,23 +1,38 @@
-class MiniQuery {
-  private elements: Element[];
+//Class를 사용하여 구현
+// class MiniQuery {
+//   private elements: Element[];
 
-  constructor(selector: string, container?: Element) {
-    this.elements = Array.from(
-      (container || document).querySelectorAll(selector)
-    );
-  }
+//   constructor(selector: string, container?: Element) {
+//     this.elements = Array.from(
+//       (container || document).querySelectorAll(selector)
+//     );
+//   }
 
-  click(handler: EventListener) {
-    this.elements.forEach((element) => {
-      element.addEventListener("click", handler);
-    });
-  }
+//   click(handler: EventListener) {
+//     this.elements.forEach((element) => {
+//       element.addEventListener("click", handler);
+//     });
+//   }
 
-  length() {
-    return this.elements.length;
-  }
-}
+//   length() {
+//     return this.elements.length;
+//   }
+// }
 
 export const $ = (selector: string, container?: Element) => {
-  return new MiniQuery(selector, container);
+  // return new MiniQuery(selector, container);
+  const elements = Array.from(
+    (container || document).querySelectorAll(selector)
+  );
+
+  return {
+    length: () => {
+      return elements.length;
+    },
+    click: (handler: EventListener) => {
+      elements.forEach((element) => {
+        element.addEventListener("click", handler);
+      });
+    },
+  };
 };
